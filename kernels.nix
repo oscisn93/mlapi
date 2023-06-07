3d6861f327da9793c18fa3d58031ad4a3284f141
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-06-07 15:11:05 -0700

    Added api with poetry template

--- kernels.nix
+++ kernels.nix
@@ -1,14 +1,9 @@
-{pkgs, ...}: {
+{self, pkgs, ...}: {
   kernel.python.mlai = {
     enable = true;
     name = "python311";
     displayName = "Python version 3.11";
-
-    extraPackages = ps: [
-      ps.numpy
-      ps.pandas
-      ps.matplotlib
-    ];
+    projectDir = ./api;
   };
   kernel.haskell.fn_scriptes.enable = true;
   kernel.bash.scripting_cli.enable = true;
