97ce2e7e3bd7970714c92389da6380e086e337e8
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-06-07 16:53:41 -0700

    adding tensorflow

--- kernels.nix
+++ kernels.nix
@@ -1,9 +1,13 @@
-{self, pkgs, ...}: {
-  kernel.python.mlai = {
+{pkgs, ...}: {
+  kernel.python.mlapi = {
     enable = true;
     name = "python311";
     displayName = "Python version 3.11";
-    projectDir = ./api;
+    extraPackages = ps: [
+      ps.numpy
+      ps.image
+      ps.tensorflow-cpu
+    ];
   };
   kernel.haskell.fn_scriptes.enable = true;
   kernel.bash.scripting_cli.enable = true;
