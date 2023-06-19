1b9571884e0b4e709f3b7cfbdc41050eebfd1df5
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-06-19 09:55:47 -0700

    erased unused .env file and updated font

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 // @ts-ignore
 const Container = styled.div`
+  font-family: 'Titillium Web', sans-serif;
   width: 100%;
   height: 100vh;
   display: flex;
